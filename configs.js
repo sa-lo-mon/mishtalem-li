fddbbe8a7ff182f47031f31914748e082ad746ae
sa-lo-mon <salomon85r@gmail.com>
2015-09-01 11:40:58 +0300

    configs update

--- configs.js
+++ configs.js
@@ -2,7 +2,7 @@ var configs=
 {
   "server":"localhost",
   "port": 8080,
-  "credentials":"C:\\credentials.json",
+  "credentials":"./credentials.json",
   "ebayAppId":'nonamec97-dbe0-4791-9985-731e31e5d36'
 };
 
